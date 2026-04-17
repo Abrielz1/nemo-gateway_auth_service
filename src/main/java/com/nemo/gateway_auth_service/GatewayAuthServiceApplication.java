d486802edeac898be3a17d7a28c48f585c3412c0
Abriel <106871954+Abrielz1@users.noreply.github.com>
2026-04-17 14:13:01 +0300

    Add files via upload

---
+++ src/main/java/com/nemo/gateway_auth_service/GatewayAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nemo.gateway_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nemo/gateway_auth_service/GatewayAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemo.gateway_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
